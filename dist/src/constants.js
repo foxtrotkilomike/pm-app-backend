148f1b128c193e5e9b0fe284a808163347892e7e
Philipp Khromov <filooloog@mail.ru>
2022-11-29 17:44:01 +0300

    deploy: backend

---
+++ dist/src/constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CLUSTER_INFO = exports.PASSWORD = exports.USER_NAME = exports.SECRET_KEY = exports.PORT = void 0;
+exports.PORT = 3000;
+exports.SECRET_KEY = 'myVerySecretKey-04.05.2022';
+exports.USER_NAME = process.env.USER_NAME;
+exports.PASSWORD = process.env.PASSWORD;
+exports.CLUSTER_INFO = process.env.CLUSTER_INFO;
